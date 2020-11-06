6a47e66e342a4175a017cd359e0514e48b182a20
Maratosh <49524488+Maratosh@users.noreply.github.com>
2020-11-06 12:23:35 +0300

    Create lab2.py

---
+++ lab2.py
@@ -0,0 +1,20 @@
+# Walzenlage (Rotor position)
+# Ringstellung (Ping position) The relative position of the rotor and their internal wiring
+# Steckerverbindungen Plug connections 
+
+r = 3 # rotors
+n = 5 # 50 unique rotors
+w = 10 # wires
+p = 26 # plugboard
+
+def fac(x):
+    if x == 1:
+        return x
+    else:
+        return x*fac(x-1)
+
+Walzenlage = (fac(n)/fac(n-r))
+Ringstellung = p ** r
+Steckerverbindungen =  (fac(p)) / ( (fac(p - 2*w)) * (2**w) * (fac(w)) )
+
+t = Walzenlage * Ringstellung * Steckerverbindungen
