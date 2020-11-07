907a8f1d2ff65e7f39a8dc1781d51f27a83b5b70
Maratosh <49524488+Maratosh@users.noreply.github.com>
2020-11-08 00:13:29 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,3 +1,10 @@
+# Factorial
+def fac(x):
+    if x == 1:
+        return x
+    else:
+        return x*fac(x-1)
+#-----------------------------------------------------------------------------------------
 # Walzenlage (Rotor position)
 # Ringstellung (Ping position) The relative position of the rotor and their internal wiring
 # Steckerverbindungen Plug connections 
@@ -7,14 +14,10 @@ n = 5 # 50 unique rotors
 w = 10 # wires
 p = 26 # plugboard
 
-def fac(x):
-    if x == 1:
-        return x
-    else:
-        return x*fac(x-1)
+
 
 Walzenlage = (fac(n)/fac(n-r))
-Ringstellung = p ** r
+Ringstellung = p ** (r - 1)
 Steckerverbindungen =  (fac(p)) / ( (fac(p - 2*w)) * (2**w) * (fac(w)) )
 
 t = Walzenlage * Ringstellung * Steckerverbindungen
