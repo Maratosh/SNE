48d53680fd969b59fe4ed2bcd82b5f1517740826
Maratosh <49524488+Maratosh@users.noreply.github.com>
2020-11-08 02:55:00 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,23 +1,36 @@
+#!/usr/bin/python3
 # Factorial
 def fac(x):
     if x == 1:
         return x
     else:
         return x*fac(x-1)
-#-----------------------------------------------------------------------------------------
-# Walzenlage (Rotor position)
-# Ringstellung (Ping position) The relative position of the rotor and their internal wiring
-# Steckerverbindungen Plug connections 
+#######################
+#(Walzenlage). Possible ways of selecting and ordering the rotors
+#(Ringstellung). Possible ways to install rings around rotors.
+#(Kenngruppen). Possible ways to position of the rotors in the machine.
+#(Steckerverbindungen) Possible ways of connecting letters to each other with plugs.
 
 r = 3 # rotors
-n = 5 # 50 unique rotors
+n = 5 # unique rotors
 w = 10 # wires
-p = 26 # plugboard
+p = 26 # alphabet
 
 
 
 Walzenlage = (fac(n)/fac(n-r))
 Ringstellung = p ** (r - 1)
+Kenngruppen = p ** r
 Steckerverbindungen =  (fac(p)) / ( (fac(p - 2*w)) * (2**w) * (fac(w)) )
 
-t = Walzenlage * Ringstellung * Steckerverbindungen
+total = Walzenlage * Ringstellung * Kenngruppen * Steckerverbindungen
+
+print('Possible ways of selecting and ordering the rotors', int(Walzenlage))
+print('Possible ways to install rings around rotors', Ringstellung)
+print('Possible ways to position of the rotors in the machine', Kenngruppen)
+print('Possible ways of connecting letters to each other with plugs', Steckerverbindungen)
+print('total probabilities',  total)
+
+# the number of the plugboard cables at which the maximum number of probabilities of the Possible ways of connecting letters to each other
+#for i in range (1, 15):
+#    print(i, 'pairs', (fac(30)) / ( (fac(30 - 2* i)) * (2**i) * (fac(i)) ))
